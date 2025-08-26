8d4d26560b9fddff568059595233257b5e8b2188
maximart <maxime.martinez96@hotmail.fr>
2025-08-26 14:40:28 +0200

    feat(tests): add some commands to resolve problem error 2002

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -15,7 +15,7 @@ while ! mysqladmin ping --silent 2>/dev/null; do
 done
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" # Create Database
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;"
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;"
 mysql -e "FLUSH PRIVILEGES;"
 
