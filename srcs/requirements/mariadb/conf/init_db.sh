24922f84ee8aaa03e77b64d5ab205f739b9b2deb
maximart <maxime.martinez96@hotmail.fr>
2025-08-26 12:59:02 +0200

    feat(run): add some commands and script to mariadb configuration

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-mysql -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+set -e # if error, stop everything
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql # Init BDD for the first time
+
+mysqld --user=mysql & # Background server
+
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" # Create Database
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+echo " MariaDB is ready"
+
+wait # Container still running
