cebce7409da1c0e49ce8d60ae3763e76c5f46cbe
maximart <maxime.martinez96@hotmail.fr>
2025-09-03 19:08:57 +0200

    feat(configuration): change the env variable and add secrets

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -2,6 +2,7 @@
 
 set -e # if error, stop everything
 
+
 if [ -f "/run/secrets/mysql_root_password" ]; then
   MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
 else
@@ -25,20 +26,26 @@ else
   echo "BDD already exist"
 fi
 
+FIRST_RUN=false
 mysqld --user=mysql --datadir=/var/lib/mysql & # Background server
-
+FIRST_RUN=true
 while ! mysqladmin ping --silent 2>/dev/null; do
     echo "Server waiting..."
     sleep 1
 done
-
-mysql << EOF
-ALTER USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+if [ "$FIRST_RUN" = true]; then
+  echo "Init configuration"
+  mysql << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
+  echo "Init finished"
+else
+  echo "DB already exist, starting normaly"
+fi
 
 echo " MariaDB is ready"
 
